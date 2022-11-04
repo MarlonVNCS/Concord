c99348a5d6e741308bde0d89cebec9671ad7bfbc
MarlonVNCS <marlon-vinicius@live.com>
2022-11-04 09:24:36 -0300

    Voz

--- app/src/main/java/com/example/concord/MainActivity.java
+++ app/src/main/java/com/example/concord/MainActivity.java
@@ -35,6 +35,21 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
+//Voz
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.ChannelMediaOptions;
+
 
 public class MainActivity extends AppCompatActivity {
     public static String nome;
@@ -56,10 +71,134 @@ public class MainActivity extends AppCompatActivity {
 
     private Context context;
 
+    //Voz
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS =
+            {
+                    Manifest.permission.RECORD_AUDIO
+            };
+
+    private boolean checkSelfPermission()
+    {
+        if (ContextCompat.checkSelfPermission(this, REQUESTED_PERMISSIONS[0]) !=  PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void showMessage(String message) {
+        runOnUiThread(() ->
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show());
+    }
+
+    // Fill the App ID of your project generated on Agora Console.
+    private final String appId = "11286fbd9bad4255ad81a8bc1ae74720";
+    // Fill the channel name.
+    private String channelName = "Canal";
+    // Fill the temp token generated on Agora Console.
+    private String token = "007eJxTYHD9MXHvq7xJUUH3onp49q9If6PRU7F1o9fSGY2MAg4fD5YrMBgaGlmYpSWlWCYlppgYmZomplgYJlokJRsmppqbmBsZHP+WktwQyMgwIzODiZEBAkF8VgbnxLzEHAYGAO8yIWA=";
+    // An integer that identifies the local user.
+    private int uid = 0;
+    // Track the status of your connection
+    private boolean isJoined = false;
+
+    // Agora engine instance
+    private RtcEngine agoraEngine;
+    // UI elements
+    private TextView infoText;
+    private Button joinLeaveButton;
+
+    private void setupVoiceSDKEngine() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            agoraEngine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Check the error.");
+        }
+    }
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote user joining the channel.
+        public void onUserJoined(int uid, int elapsed) {
+            runOnUiThread(()->infoText.setText("Remote user joined: " + uid));
+        }
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            // Successfully joined a channel
+            isJoined = true;
+            showMessage("Joined Channel " + channel);
+            runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            // Listen for remote users leaving the channel
+            showMessage("Remote user offline " + uid + " " + reason);
+            if (isJoined) runOnUiThread(()->infoText.setText("Waiting for a remote user to join"));
+        }
+
+        @Override
+        public void onLeaveChannel(RtcStats 	stats) {
+            // Listen for the local user leaving the channel
+            runOnUiThread(()->infoText.setText("Press the button to join a channel"));
+            isJoined = false;
+        }
+    };
+
+    private void joinChannel() {
+        ChannelMediaOptions options = new ChannelMediaOptions();
+        options.autoSubscribeAudio = true;
+        // Set both clients as the BROADCASTER.
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        // Set the channel profile as BROADCASTING.
+        options.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        agoraEngine.joinChannel(token, channelName, uid, options);
+    }
+
+    public void joinLeaveChannel(View view) {
+        if (isJoined) {
+            agoraEngine.leaveChannel();
+            joinLeaveButton.setText("Join");
+        } else {
+            joinChannel();
+            joinLeaveButton.setText("Leave");
+        }
+    }
+
+    // Acabou voz
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Voz
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // If all the permissions are granted, initialize the RtcEngine object and join a channel.
+        if (!checkSelfPermission()) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, PERMISSION_REQ_ID);
+        }
+
+        setupVoiceSDKEngine();
+
+        // Set up access to the UI elements
+        joinLeaveButton = findViewById(R.id.joinLeaveButton);
+        infoText = findViewById(R.id.infoText);
+
+        //Acabou voz
+
+        //super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         send = findViewById(R.id.fab_send);
         message = findViewById(R.id.message);
@@ -90,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
                 String msg = message.getEditText().getText().toString();
                 msg_valid = checkMsg(msg);
                 if (!msg_valid){
-                    Toast.makeText(MainActivity.this, "Escreve alguma coisa porra", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Escreve alguma coisa...", Toast.LENGTH_SHORT).show();
                 } else {
                     //Log.i("mensage",msg);
                     //Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
@@ -121,6 +260,21 @@ public class MainActivity extends AppCompatActivity {
                Toast.makeText(context, "Copiado", Toast.LENGTH_SHORT).show();
             }});*/
     }
+
+    //Voz
+
+    protected void onDestroy() {
+        agoraEngine.leaveChannel();
+        super.onDestroy();
+
+        // Destroy the engine in a sub-thread to avoid congestion
+        new Thread(() -> {
+            RtcEngine.destroy();
+            agoraEngine = null;
+        }).start();
+    }
+
+    //Acabou voz
 
     //meu codigo
     public void exit(View view){
